ac6778ecfece625f22581dd5407a248f8571c38c
Karen Valier <karenvalierpalmero@gmail.com>
2022-11-04 12:43:10 -0600

    agregue label y button

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
  import {showGraphic} from './chart.js'
+ import {getValue} from './select.js'
 
  let priceList = []
  const bpiPrice = async()=>{
     const API = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     const resp= await fetch(API,{method:'GET'})
     const prices= await resp.json();
-    // console.log(prices)
+    console.log(prices)
     
     const euro = prices.bpi.EUR.rate;
     const dolar = prices.bpi.USD.rate;
@@ -14,5 +15,5 @@
     
     showGraphic(priceList)
 }
-
 bpiPrice()
+
